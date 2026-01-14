33124d1ae91101205ffaaa7a73c89ccb1cc82830
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-14 20:47:47 -0300

    Atualização de Strings

--- Logica_Programacao_Basica/Parte6_String.py
+++ Logica_Programacao_Basica/Parte6_String.py
@@ -1,16 +1,55 @@
 #==========Strings==========
 
+def calculo(valor):
+    return valor * 100
+
 nome = 'Andressa'
 sobrenome = 'Monteiro'
 altura = 1.58687
 
-#Formatacao com f-string
-linha_1 = f'{nome} {sobrenome} tem {altura:.3f}'
-print(linha_1)
+print("Nome: " + nome + sobrenome + "Altura:" + str(altura))
 
 #Formatacao com format
+print("Format")
 linha_2 = '{} {} tem {:.3f}'.format(nome, sobrenome, altura)
 linha_3 = '{0} {1} tem {2:.3f}'.format(nome, sobrenome, altura)
 
 print(linha_2)
 print(linha_3)
+print("\n")
+
+#Formatacao com f-string
+print("f-string")
+
+linha_1 = f'{nome} {sobrenome} tem {altura:.3f}'
+print(linha_1)
+
+preco = 5
+taxa = 2.5
+
+texto = f"O preço é {preco} reais."
+print(texto)
+
+texto = f"O preço é {preco:.2f} reais."
+print(texto)
+
+texto = f"O preço é {59:.2f} reais."
+print(texto)
+
+texto = f"O preço é {59*16} reais."
+print(texto)
+
+texto = f"O preço é {preco+(preco*taxa)} reais."
+print(texto)
+
+texto = f"É muito {'caro' if preco>10 else 'barato'}."
+print(texto)
+
+filme = "Esqueceram de mim"
+texto = f"Meu filme favorito é {filme.upper()}."
+print(texto)
+
+texto = f"Teste de funções com string: {calculo(98)}."
+print(texto)
+
+
