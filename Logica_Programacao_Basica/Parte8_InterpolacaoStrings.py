445defb025ff156dea51a25d7053ec9d6ffdafe6
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-15 11:24:38 -0300

    Update Parte8_InterpolacaoStrings.py

--- Logica_Programacao_Basica/Parte8_InterpolacaoStrings.py
+++ Logica_Programacao_Basica/Parte8_InterpolacaoStrings.py
@@ -3,7 +3,7 @@
 # s - string
 # d e i - int
 # f - float
-# x e X - Hexadecimal (ABCDEF0123456789)
+# x e X - hexadecimal (ABCDEF0123456789)
 
 
 nome = 'Luiz'
@@ -26,7 +26,7 @@ print('Meu nome é %s, sou %s, tenho %d e %.1f' % (nome2,cargo,idade,altura))
 # d - int
 # f - float
 # .<número de dígitos>f
-# x ou X - Hexadecimal
+# x ou X - hexadecimal
 
 # (Caractere)(><^)(quantidade) -> Preencher com char
 # > - Esquerda
@@ -38,6 +38,7 @@ print('Meu nome é %s, sou %s, tenho %d e %.1f' % (nome2,cargo,idade,altura))
 # Conversion flags - !r !s !a
 
 variavel = 'HOJE'
+print("Formatação")
 print(f'{variavel}')
 print(f'{variavel: >10}.') #Adiciona 10 caracteres na esquerda
 print(f'{variavel: <10}.') #Adiciona 10 caracteres na direita
@@ -46,7 +47,6 @@ print(f'{variavel: ^10}.')
 
 print(f'{8534.487364746:0=+10,.1f}')
 #ChatGPT
-
 # 0: Preenche o espaço à esquerda do número com zeros, se necessário.
 # =: Coloca o sinal (+ ou -) logo após o preenchimento de zeros, antes do número em si.
 # +: Inclui o sinal positivo + para números positivos e o sinal negativo - para números negativos.
@@ -59,7 +59,7 @@ print(f'O hexadecimal de 1500 é {1500:08X}')
 print(f'{variavel!r}')
 
 #==========Fatiamento==========
-# Fatiamento de strings
+# Fatiamento de strings (Slicing)
 #  012345678
 #  Olá mundo
 # -987654321
@@ -68,5 +68,19 @@ print(f'{variavel!r}')
 # len() -> retorna a quantidade de caracteres da string
 
 variavel = 'Olá mundo' #Manipulacao de vetores
-print(variavel[::-1])
+print(variavel[::-1]) #Step over from the back
 print(len(variavel))
+
+variavel2 = '0123456789' #Manipulacao de vetores
+print(variavel2[::-1]) #Step over from the back
+print(len(variavel2))
+
+#[start:stop:stepover]
+palavra = '0123456789'
+print("Palavra")
+print(palavra[0]) #0
+print(palavra[1:5]) #1234
+print(palavra[1:5:2]) #13
+print(palavra[::]) #0123456789
+print(palavra[2:]) #23456789
+print(palavra[:2]) #01
