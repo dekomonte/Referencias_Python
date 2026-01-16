045e84ff80d5f2cc1ce38dc04f99a0c6320dc76e
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-16 11:54:41 -0300

    Create python_string_methods.py

---
+++ Logica_Programacao_Basica/python_string_methods.py
@@ -0,0 +1,28 @@
+#Python String Methods
+
+palavra = 'hoje esTá Quente em Brasília, Fazendo 9654 mil GrAus de TemPeRaTuRa'
+print("String: ",palavra)
+
+print("casefold")
+print(palavra.casefold())
+
+print("capitalize")
+print(palavra.capitalize())
+
+print("count")
+print(palavra.count('e'))
+
+print("upper")
+print(palavra.upper())
+
+print("lower")
+print(palavra.lower())
+
+print("swapcase")
+print(palavra.swapcase())
+
+print("replace")
+print(palavra.replace('Quente', 'embaixo da mesa'))
+
+print("split")
+print(palavra.split(','))
