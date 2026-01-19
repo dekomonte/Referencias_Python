76823449d6bf40d15d27115fbfbc8329ef676b1d
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-19 10:48:26 -0300

    Update Parte10_EstruturasRepeticao.py

--- Logica_Programacao_Basica/Parte10_EstruturasRepeticao.py
+++ Logica_Programacao_Basica/Parte10_EstruturasRepeticao.py
@@ -1,5 +1,7 @@
 #==========Estruturas de Repeticao==========
-#WHILE
+#WHILE -> Executa ações enquanto uma condição for verdadeira
+
+#Exemplo 1
 condicao = True
 while condicao:
     print(1)
@@ -7,9 +9,18 @@ while condicao:
     print(3)
     condicao = False
 
+#Exemplo 2
 cont = 0
 while cont < 17:
     print("Saudades da minha amiga")
     print(cont)
     cont+=1
-
+    
+#Exemplo 3
+i = 0
+while i < 5:
+    print(i)
+    if i == 3:
+        print("Vou sair do loop com break, i não vai chegar no 5")
+        break #Sai do loop antes da condição ser ficar falsa
+    i+=1
