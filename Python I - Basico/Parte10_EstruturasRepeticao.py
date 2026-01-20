5fb25872e9e2d5dfe082e53a7b4df0da398de340
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-20 11:48:19 -0300

    Update Parte10_EstruturasRepeticao.py

--- Python I - Basico/Parte10_EstruturasRepeticao.py
+++ Python I - Basico/Parte10_EstruturasRepeticao.py
@@ -2,6 +2,7 @@
 #WHILE -> Executa ações enquanto uma condição for verdadeira
 
 #Exemplo 1
+print("===== Exemplo 1 =====")
 condicao = True
 while condicao:
     print(1)
@@ -10,13 +11,14 @@ while condicao:
     condicao = False
 
 #Exemplo 2
+print("===== Exemplo 2 =====")
 cont = 0
-while cont < 17:
-    print("Saudades da minha amiga")
-    print(cont)
+while cont < 15:
+    print(f"Bora tubarões! + {cont}")
     cont+=1
     
 #Exemplo 3
+print("===== Exemplo 3 =====")
 i = 0
 while i < 5:
     print(i)
@@ -24,7 +26,25 @@ while i < 5:
         print("Vou sair do loop com break, i não vai chegar no 5")
         break #Sai do loop antes da condição ser ficar falsa
     i+=1
+    
+#Exemplo 4
+print("===== Exemplo 4 =====")
+i = 0
+while i < 5:
+    i+=1
+    if i == 3:
+        print("Vou entrar aqui e retornar para o início do loop")
+        continue #Pula para a próxima iteração
+    print(i)
+    
 
-
-#Operadores de atribuição
-#= += -= *= /= //= **= %=
+#Exemplo 5
+print("===== Exemplo 5 =====")
+linhas=1
+while linhas < 5:
+    colunas=1
+    while colunas < 5:
+        print(f"linha:{linhas} e coluna:{colunas}")
+        colunas+=1
+    linhas+=1
+    
