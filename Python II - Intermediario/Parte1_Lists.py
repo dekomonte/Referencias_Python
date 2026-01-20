3b5ed961f40fda32271c239c1231c79c8ba23130
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-20 14:13:50 -0300

    Update Parte1_Lists.py

--- Python II - Intermediario/Parte1_Lists.py
+++ Python II - Intermediario/Parte1_Lists.py
@@ -8,3 +8,23 @@ li3 = [1, 'a', True]
 print(li)
 print(li2)
 print(li3)
+
+#List Slicing
+carrinho_compras = ['notebooks', 'brinquedos', 'frutas', 'verduras','xampu', 'panela']
+print(carrinho_compras[0:2])
+carrinho_compras[1] = 'carne'
+print(carrinho_compras[0::2])
+
+print("Caso 1:")
+frutas = ['banana','uva','kiwi', 'melão', 'maçã', 'abacaxi']
+novo_frutas = frutas #Atribui
+novo_frutas[2] = 'limão' #fruta também é modificada
+print(frutas)
+print(novo_frutas)
+
+print("Caso 2:")
+frutas = ['banana','uva','kiwi', 'melão', 'maçã', 'abacaxi']
+novo_frutas = frutas[:] #Copia toda lista
+novo_frutas[2] = 'limão' #fruta não é modificada
+print(frutas)
+print(novo_frutas)
