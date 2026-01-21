211971ec71de472daff58778cbf728f0b7845f4c
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-21 13:51:16 -0300

    Create Parte2_ListsMethods.py

---
+++ Python II - Intermediario/Parte2_ListsMethods.py
@@ -0,0 +1,46 @@
+#==========#Data Structure==========
+
+#Lists Methods
+lista = [1,2,3,4,5,6,7,8,9,10]
+print(lista)
+
+#adding
+lista.append(100) #	Adds an element at the end of the list
+print(lista)
+
+lista.insert(3,'aqui') # Adds an element at the specified position
+print(lista)
+
+lista2 = ['arroz','feijao']
+lista.extend(lista2) # Add the elements of a list to the end of the current list
+print(lista)
+
+#removing
+lista.pop() # Removes the element at the specified position - default: the last item on the list
+print(lista)
+
+lista.pop(0) # Removes the element at the specified position
+print(lista)
+
+lista.remove(3) # Removes the FIRST item with the specified value
+print(lista)
+
+lista.clear() # Removes all the elements from the list
+print(lista)
+
+#Lists Methods 2
+lista = [1,2,3,4,5,6,7,8,9,10]
+print("Parte 2: ", lista)
+
+lista3 = ['a', 'r', 'j', 'v', 'e', 't']
+
+#index
+print(lista.index(3)) # Qual o index do elemento 
+
+print(lista3)
+print(lista3.index('t')) # Returns the index of the first element with the specified value
+
+print('c' in lista3) # Check if a value is present in a sequence
+print('e' in lista3)
+
+print(lista.count(4)) # Returns the number of elements with the specified value
