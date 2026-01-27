9fbf3c5404a25c6d958cbb58dea1187bb2e6563a
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-01-27 11:27:44 -0300

    Create Parte4_Dictionaries.py

---
+++ Python II - Intermediario/Parte4_Dictionaries.py
@@ -0,0 +1,50 @@
+#==========#Data Structure==========
+
+#Dictionary
+#It might be called a hash table, maybe map or objects in anothers languages
+#Is a unordered key value pair
+
+dictionary = {
+    'a': 1, #key-value
+    'b': 2
+}
+
+print(dictionary['b'])
+
+#Exemplo 1
+dicionario1 = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+
+print(dicionario1["brand"])
+print(dicionario1['year'])
+print(dicionario1['electric'])
+print(dicionario1['colors'])
+
+#Exemplo 2
+lista = [
+    {
+        'a': [1,2,3],
+        'b': [4,5,6],
+        'x': True
+    },
+    {
+        'a': [1,2,3],
+        'b': 'oioioi',
+        'x': False,
+        'teste': 'teste'
+    }
+]
+
+print(lista[0]['a'])
+print(lista[0]['b'])
+print(lista[0]['x'])
+
+print(lista[1]['a'])
+print(lista[1]['b'])
+print(lista[1]['x'])
+print(lista[1]['teste'])
