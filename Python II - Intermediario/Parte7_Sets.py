28fdd6bf2e67556738d087b8772d4bf48784029e
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-03-20 11:56:20 -0300

    Update Parte7_Sets.py

--- Python II - Intermediario/Parte7_Sets.py
+++ Python II - Intermediario/Parte7_Sets.py
@@ -3,6 +3,7 @@
 #Set
 #Unordered collections of unique objects
 
+#Exemplo 1
 my_set = {1,2,3,4,5,6}
 print(my_set)
 
@@ -16,3 +17,19 @@ print(my_set)
 
 minha_lista = [1,2,3,4,4,4,5,6]
 print(set(minha_lista))
+
+#Exemplo 2
+my_set2 = {"laranja","maçã","arroz","Flamengo", 1, 44, False}
+print(my_set2)
+print("len: ",len(my_set2))
+
+#Exemplo 3
+conjunto1 = {1,2,3,4,5}
+conjunto2 = {4,5,6,7,8,9,10,11,12,13,14,15}
+
+print("difference(): ",conjunto1.difference(conjunto2))
+
+conjunto2.discard(7)
+print("discard(): ", conjunto2)
+
+print("intersection(): ",conjunto1.intersection(conjunto2))
