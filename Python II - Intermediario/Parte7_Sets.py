83147a613f1817cf1d34b4aea981714b909baa47
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-03-20 11:38:13 -0300

    Create Parte7_Sets.py

---
+++ Python II - Intermediario/Parte7_Sets.py
@@ -0,0 +1,18 @@
+#==========#Data Structure==========
+
+#Set
+#Unordered collections of unique objects
+
+my_set = {1,2,3,4,5,6}
+print(my_set)
+
+my_set = {1,2,3,4,4,4,5,6} #Tudo é único
+print(my_set)
+
+my_set.add(4)
+my_set.add(5)
+my_set.add(100)
+print(my_set)
+
+minha_lista = [1,2,3,4,4,4,5,6]
+print(set(minha_lista))
