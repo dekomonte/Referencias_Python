16d69c3487372566997dcc096c35c22e5f6d9e4c
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-04-20 15:18:46 -0300

    Create horarios_medicamentos.py

---
+++ Aplicacoes_Exemplos_Exercicios/horarios_medicamentos.py
@@ -0,0 +1,15 @@
+# Horários para tomar remédio - script mais tosco e nada robusto possível
+
+# remedio = "Ondansetrona"
+# hora = 9
+# intervalo = 8
+
+remedio = "Toragesic"
+hora = 6
+intervalo = 8 
+
+print(remedio)
+while (hora <= 24):
+    #print(hora,'h')
+    print(hora)
+    hora = hora+intervalo
