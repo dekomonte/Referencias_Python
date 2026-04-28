9f814520e46b02981adfa0089e99a0877d5ff862
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-04-28 17:42:42 -0300

    Update Exercicio5.py

--- Aplicacoes_Exemplos_Exercicios/Exercicio5.py
+++ Aplicacoes_Exemplos_Exercicios/Exercicio5.py
@@ -15,4 +15,23 @@ for x in picture:
         # if y == 1: print("*", end="")
         else: print("*", end="") #Assumindo que picture será sempre 0 ou 1
     print(" ")
+print("\n")
 
+# EXTRA - Inverter a imagem (sugestão do Lucas)
+print("===== Árvore Invertida =====")
+tamanho = len(picture)
+# print(tamanho)
+for i in range(tamanho-1,-1,-1):
+    for item in picture[i]:
+        if item == 0: print(" ", end="")
+        # if y == 1: print("*", end="")
+        else: print("*", end="") #Assumindo que picture será sempre 0 ou 1
+    print(" ")
+
+print("===== Árvore Invertida (Lucas) =====")
+for x in picture[::-1]:
+    for y in x:
+        if y == 0: print(" ", end="")
+        # if y == 1: print("*", end="")
+        else: print("*", end="") #Assumindo que picture será sempre 0 ou 1
+    print(" ")
