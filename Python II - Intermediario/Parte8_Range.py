7f8303cd78cdb7fc65495d9b5c4c22c1ae0cb9f5
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-04-28 15:42:09 -0300

    Create Parte8_Range.py

---
+++ Python II - Intermediario/Parte8_Range.py
@@ -0,0 +1,23 @@
+print("Exemplo 1")
+for i in range(0,10):
+    print(i)
+
+print("Exemplo 2")
+for _ in range(0,10):
+    print(_)
+
+print("Exemplo 3")
+for _ in range(0,10,2):
+    print(_)
+
+print("Exemplo 4")
+for _ in range(10,0,-2):
+    print(_)
+
+print("Exemplo 5")
+for _ in range(10,0,-2):
+    print(list(range(10)))
+
+print("Exemplo 6")
+for _ in range(2):
+    print(list(range(10)))
