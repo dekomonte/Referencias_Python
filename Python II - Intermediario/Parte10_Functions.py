b28e09272153b0d768232152f9c27d816075837d
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-05-22 14:25:36 -0300

    Create Parte10_Functions.py

---
+++ Python II - Intermediario/Parte10_Functions.py
@@ -0,0 +1,9 @@
+def minha_funcao_de_dizer_oi():
+    print("Oi")
+
+# Function are useful when you have things that you want to do over and over
+minha_funcao_de_dizer_oi()
+
+for i in range(10):
+    minha_funcao_de_dizer_oi()
+
