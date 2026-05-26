101fe3690e58c0c06b8a4f94e7eb6d6d5668d79f
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-05-26 17:03:09 -0300

    Create Parte1.py

---
+++ Python III - POO/Parte1.py
@@ -0,0 +1,22 @@
+#OOP
+
+#Everything in Python is an object
+#Objects have methods and attributes that we can access with the DOT method
+
+print(type('objeto'))
+print(type(64))
+print(type(7.9))
+print(type(True))
+print(type([]))
+print(type({}))
+print(type(()))
+
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'bool'>
+# <class 'list'>
+# <class 'dict'>
+# <class 'tuple'>
+
+#Paradigm 
