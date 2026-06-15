071f8b4bd482f39e50ce21c11b57a7be3b722252
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-06-15 14:32:19 -0300

    Update Parte1.py

--- Python IV - Avançado/Parte1.py
+++ Python IV - Avançado/Parte1.py
@@ -1,1 +1,10 @@
-print("Olá")
+# ===== Functional Programming =====
+
+#Paradigma de programação
+#Funções matemáticas puras
+
+#Pilares
+#1 - Funções Puras
+#2 - Imutabilidade
+#3 - Funções de Primeira Classe e Alta Ordem
+#4 - Composição de Funções
